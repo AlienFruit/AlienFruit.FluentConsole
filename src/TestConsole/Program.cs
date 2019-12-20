1a26c4b74607b6b94ee4e389ab5245cdad2ae149
AlienFruit <alienfruit@ya.ru>
2019-12-20 22:51:49 +0200

    feat: add test console project

---
+++ src/TestConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using AlienFruit.FluentConsole;
+using AlienFruit.FluentConsole.AsciiArt;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FConsole.GetInstance().GetAsciiArtBuilder()
+                .DrawDemo(DemoPicture.DarthVader);
+
+            FConsole.GetInstance().GetAsciiArtBuilder()
+                .DrawDemo(DemoPicture.ApertureScience);
+
+            FConsole.GetInstance().GetAsciiArtBuilder()
+                .DrawDemo(DemoPicture.Alienfruit);
+
+            FConsole.ReadKey();
+        }
+    }
+}
